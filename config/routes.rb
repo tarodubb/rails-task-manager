27d77274b6ce798d91c64b370b2f921c7f4423bd
maxwell wong <midorikawamaxwell@gmail.com>
2023-01-07 16:46:18 +0900

    created a task list manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Show all tasks
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  # See details about one task
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # Create a task
+  post 'tasks', to: 'tasks#create'
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  # Destroy a task
+  delete 'tasks/:id', to: "tasks#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+puts 'Destroying all tasks...'
+Task.destroy_all
+puts 'Done!'
+
+Task.create(title: 'Laundry', details: 'Do not mix colors!')
+Task.create(title: 'Studying', details: 'A lot of flashcards to do', completed: true)
+Task.create(title: 'Testing', details: 'Test your code!', completed: true)
+Task.create(title: 'Checking', details: 'Check your code!')
+puts 'Done!'
+
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
